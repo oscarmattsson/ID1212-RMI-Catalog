57babd77985e06eede11696e4e057fd5cbd3180b
Oscar Mattsson <oscar_mattsson@live.se>
2017-12-04 03:54:10 +0100

    Changed file size to long

--- src/main/java/oscarmat/kth/id1212/rmicatalog/comon/FileDTO.java
+++ src/main/java/oscarmat/kth/id1212/rmicatalog/comon/FileDTO.java
@@ -2,21 +2,37 @@ package oscarmat.kth.id1212.rmicatalog.comon;
 
 import java.io.Serializable;
 
-public class File implements Serializable {
+public class FileDTO implements Serializable {
 
     private String name;
-    private int size;
+    private long size;
     private boolean isPublic;
     private boolean isReadonly;
 
-    public File(String name, int size, boolean isPublic, boolean isReadonly) {
+    public FileDTO(String name, long size, boolean isPublic, boolean isReadonly) {
         this.name = name;
         this.size = size;
         this.isPublic = isPublic;
         this.isReadonly = isReadonly;
     }
 
-    public File(String name, int size) {
+    public FileDTO(String name, long size) {
         this(name, size, false, true);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public boolean isPublic() {
+        return isPublic;
+    }
+
+    public boolean isReadonly() {
+        return isReadonly;
     }
 }
