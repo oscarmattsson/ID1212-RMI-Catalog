b06cbe8999ab2138c057941e46c6d1991ef83879
Oscar Mattsson <oscar_mattsson@live.se>
2017-12-04 03:48:59 +0100

    Added database connection to startup

--- src/main/java/oscarmat/kth/id1212/rmicatalog/server/startup/Server.java
+++ src/main/java/oscarmat/kth/id1212/rmicatalog/server/startup/Server.java
@@ -1,6 +1,9 @@
 package oscarmat.kth.id1212.rmicatalog.server.startup;
 
 import oscarmat.kth.id1212.rmicatalog.server.controller.Controller;
+import oscarmat.kth.id1212.rmicatalog.server.integration.DBHandler;
+import oscarmat.kth.id1212.rmicatalog.server.integration.UserDAO;
+import oscarmat.kth.id1212.rmicatalog.server.model.User;
 import oscarmat.kth.id1212.rmicatalog.server.view.Logger;
 
 import java.net.MalformedURLException;
@@ -8,15 +11,20 @@ import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.logging.Level;
 
 public class Server {
 
     public static void main(String[] args) {
+
         Logger logger = new Logger(System.out);
+        logger.putInfo("Starting server...");
+
+        DBHandler db = new DBHandler();
 
         try {
             new Server().startRegistry();
-            Naming.rebind(Controller.SERVER_URI, new Controller(logger));
+            Naming.rebind(Controller.SERVER_URI, new Controller(logger, db));
             logger.putInfo("Server is running.");
         }
         catch (MalformedURLException | RemoteException e) {
