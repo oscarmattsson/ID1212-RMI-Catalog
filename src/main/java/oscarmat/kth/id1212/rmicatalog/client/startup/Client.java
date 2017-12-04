3b39046dcfc065e141fdf7dfea5b61f1ad39632c
Oscar Mattsson <oscar_mattsson@live.se>
2017-12-04 03:52:45 +0100

    Startup exception

--- src/main/java/oscarmat/kth/id1212/rmicatalog/client/startup/Client.java
+++ src/main/java/oscarmat/kth/id1212/rmicatalog/client/startup/Client.java
@@ -3,11 +3,18 @@ package oscarmat.kth.id1212.rmicatalog.client.startup;
 import oscarmat.kth.id1212.rmicatalog.client.controller.Controller;
 import oscarmat.kth.id1212.rmicatalog.client.view.ClientCommandView;
 
+import java.rmi.RemoteException;
+
 public class Client {
 
     public static void main(String[] args) {
-        Controller controller = new Controller();
-        ClientCommandView view = new ClientCommandView(System.in, System.out, controller);
+        try {
+            Controller controller = new Controller();
+            ClientCommandView view = new ClientCommandView(System.in, System.out, controller);
+        }
+        catch (RemoteException e) {
+            System.err.println("Error creating controller, shutting down application.");
+        }
     }
 
 }
